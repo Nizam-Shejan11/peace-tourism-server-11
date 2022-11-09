e2af031dda43ef7bdf1056bc8153c00729af85b8
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-11-09 19:40:26 +0600

    post method added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 7000;
 require("dotenv").config();
@@ -16,17 +16,44 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+//
 
 async function run() {
   try {
     const serviceCollection = client
       .db("peaceTourismDB")
       .collection("services");
+
+    const bookingCollection = client
+      .db("peaceTourismDB")
+      .collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // bookings api
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch((error) => console.error(error));
 
+//
 app.get("/", (req, res) => {
   res.send("Hello from peace tourism");
 });
