1b49e01a3a98effa9baee935a50be051a49a93df
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-11-10 16:05:28 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 7000;
@@ -27,6 +28,15 @@ async function run() {
     const bookingCollection = client
       .db("peaceTourismDB")
       .collection("bookings");
+
+    //  JWT method
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
 
     // _ _ _ _ GET method
     app.get("/services", async (req, res) => {
